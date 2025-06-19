caaa21bba43ca141b8cb4c72a6f9e3f2073bcea0
RUTHWIK <ruthwik4566@gmail.com>
2025-06-19 20:12:17 +0530

    added files

--- App.js
+++ App.js
@@ -1,15 +1,56 @@
-const parent = React.createElement("div", { id: "parentttt" }, [
-  React.createElement("div", { id: "cc1", key: "div1" }, [
-    React.createElement("h1", { key: "h1-1" }, "hai h231"),
-    React.createElement("h2", { key: "h2-1" }, "hai h2"),
-  ]),
-  React.createElement("div", { id: "cc2", key: "div2" }, [
-    React.createElement("h1", { key: "h1-2" }, "hai im h1"),
-    React.createElement("h2", { key: "h2-2" }, "hai im h2"),
-  ]),
-]);
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-console.log("I'm here");
+//const heading=React.createElement("h1",{id:"heading"},"Hello world from react");
+//console.log(heading);
+
+//jsx is a react element
+ /*
+const jsxHeading = (
+  <h1 className="heading" tabIndex="1">
+    Bro i don't care
+  </h1>
+);*/
+//console.log(jsxHeading);
+
+//compnents should strt with capital letter
+
+//const Title = () => {
+//  return <h1>BOSS:Hello😁</h1>;
+//};
+const elem=<span>React Elem</span>
+
+
+const Title = function(){
+  return (
+    <div>
+      <h1>{elem}</h1>
+      <h1>BOSS:Hello💕</h1>
+    </div>
+  );
+  
+};
+const num=100;
+//componentcomposition <Title></Title> or <Title/> or {Title()}
+//anything inside {} is treated as a JavaScript expression.
+const HeadingComponent = () => (
+  <div id="container">
+    <Title/>
+    <h2>{num + 43}</h2>
+    <h2>{console.log("GADDAM RUTHWIK")}</h2>
+    <h1 className="heading">MAL:Bossss!😒</h1>
+  </div>
+);
+
+//jsxHeading is transpiled into js (Ecma Script-ES6) by parcel before it reachss the JS Engine.
+//Parcel-responsibility of transpiling-Babel-JS compiler
+
+//root.render(jsxHeading);
+
+//We cannot render the component using root.render(HeadingComponent)
+//But using angular braces we can.
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+
+root.render(<HeadingComponent />);
+ 
